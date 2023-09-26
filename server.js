397f6cf0dfc3b86096dc9c9f906b8f4af98b7064
Nao <naolal30@hotmail.com>
2023-09-26 11:54:17 -0400

    fix bootstrap import files

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const {MongoClient} = require('mongodb');
+
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://naolal30:ConnectdatabasetoWebstorm100.@cluster0.ttfusik.mongodb.net/test?retryWrites=true&w=majority";
+
+
+    const client = new MongoClient(uri); // create a new MongoClient
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+// list all the databases in the cluster
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));//list each database in the cluster
+
+
+
+};
