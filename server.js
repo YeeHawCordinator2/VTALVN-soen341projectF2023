3a5486da1b5f88662e2cb966a9314d0e8b21940b
Nao <naolal30@hotmail.com>
2023-09-26 22:30:01 -0400

    issue #29 - started with the database fcts

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {MongoClient} = require('mongodb');
 
 
-async function main(){
+async function connectToDatabase(){
     /**
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
@@ -17,7 +17,9 @@ async function main(){
         await client.connect();
 
         // Make the appropriate DB calls
-        await  listDatabases(client);
+//call fct here
+
+
 
     } catch (e) {
         console.error(e);
@@ -29,12 +31,86 @@ async function main(){
 main().catch(console.error);
 
 // list all the databases in the cluster
-async function listDatabases(client){
+async  function listDatabases(client){
     databasesList = await client.db().admin().listDatabases();
 
     console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));//list each database in the cluster
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+// new fcts
+async function addNewHouse(client, listingName, price, location, seller, broker, numOfBed, furnished, buildYRS, extra, type, buildType, stories){
+
+    const house = await client.db("soen_341").collection("houses").insertOne({
+        name: listingName
+    });
+    const details = await client.db("soen_341").collection("details").insertOne({
+        house_id: house.insertedId,
+        price: price,
+        location: location,
+        numOfBed: numOfBed,
+        furnished: furnished,
+        buildYRS: buildYRS,
+        extra: extra,
+        type: type,
+        buildType: buildType,
+        stories: stories
+    });
+    const sellers = await client.db("soen_341").collection("seller").insertOne({
+        house_id: house.insertedId,
+        seller: seller,
+        broker: broker
+    });
+
+    console.log(`New listing created with the following id: ${house.insertedId}`);
+console.log(`New listing created with the following id: ${details.insertedId}`);
+
+console.log(`New listing created with the following id: ${sellers.insertedId}`);
+
+}
+
+async function addNewBroker(){  //encrypt user n pass
+
+}
+
+
+async function addNewUser(){ //encrypt user n pass
+
+
+    // will create user preferences but it will all be null for now
+}
+
+//edit fcts
+
+async function editHouse(){} // include details
+
+async function editBroker(){} //broker #, user &pass, name
+
+// maybe edit user to change pass or name or smt
+
+async function editHousePreference(){} //user
+
+
+//delete fcts
+async function deleteHouse(){}
+
+async function deleteBroker(){}
+
+async function deleteUser(){} //delete house pref with
 
 
 
-};
+
+
+//read fcts - user aggregate and pipeline
+async function readHouse(){}
+
+async function getHouseList(){}
+
+async function getHouseBYDetails(){}
+
+async function getHouseSellerAndBroker(){}
+
+async function getHousePreferences(){} //from user
+
+async function getHouseByBroker(){}
+
