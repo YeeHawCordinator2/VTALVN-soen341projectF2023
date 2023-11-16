b75cbbe2ff1351061c73ef9d81a15450802078bc
Amine-Arrachid <135894161+Amine-Arrachid@users.noreply.github.com>
2023-11-15 20:27:45 -0500

    Add files via upload

---
+++ Mortgage.js
@@ -0,0 +1,14 @@
+function computeLoan(){
+  const amount = document.getElementById('amount').value
+  const interest_rate = document.getElementById('interest_rate').value
+  const months = document.getElementById('months').value
+  const interest = (amount*(interest_rate*0.01))/months
+
+  let payment = ((amount/months) + interest).toFixed(2);
+
+  // to add coma after every 3 digits
+  payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g,",");
+document.getElementById('payment').innerHTML = `Monthly Payment = ${payment}`
+
+
+}
